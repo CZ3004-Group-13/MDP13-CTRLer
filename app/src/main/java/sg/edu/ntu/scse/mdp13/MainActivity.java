fdabc5ae1e1aecfd580e5f5bd71d689affde4095
Keane <keanecodes@gmail.com>
2021-09-25 03:44:44 +0800

    temp

--- app/src/main/java/sg/edu/ntu/scse/mdp13/MainActivity.java
+++ app/src/main/java/sg/edu/ntu/scse/mdp13/MainActivity.java
@@ -9,26 +9,43 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import sg.edu.ntu.scse.mdp13.bluetooth.BluetoothFragment;
+import sg.edu.ntu.scse.mdp13.bluetooth.PagerAdapter;
+import sg.edu.ntu.scse.mdp13.bluetooth.PagerAdapter.TabItem;
+
+
+import com.google.android.material.tabs.TabLayout;
+
+import biz.laenger.android.vpbs.BottomSheetUtils;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import sg.edu.ntu.scse.mdp13.map.GridMapCanvas;
 import sg.edu.ntu.scse.mdp13.map.BoardMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GridMapCanvas gridMapCanvas;
+    @BindView(R.id.pathGrid) GridMapCanvas gridMapCanvas;
     private BoardMap finder = new BoardMap();
+    @BindView(R.id.btn_reset) Button btnReset;
+
+    @BindView(R.id.bottom_sheet_toolbar) Toolbar bottomSheetToolbar;
+    @BindView(R.id.topTabs) TabLayout bottomSheetTabLayout; //bottom_sheet_tabs
+    @BindView(R.id.viewpager) ViewPager bottomSheetViewPager; //bottom_sheet_viewpager
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activty_main);
+        ButterKnife.bind(this);
 
-        gridMapCanvas = findViewById(R.id.pathGrid);
+        setupBottomSheet();
+
         finder = gridMapCanvas.getFinder();
-
-        Button btnReset = (Button)this.findViewById(R.id.btn_reset);
-        Button btBte = (Button)this.findViewById(R.id.btn_bte);
 
         btnReset.setOnClickListener(new OnClickListener() {
             @Override
@@ -39,12 +56,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnReset.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showBottomSheetDialog();
-            }
-        });
+
+
+//        btBte.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                showBottomSheetDialog();
+//            }
+//        });
     }
 
     private void makeToast(boolean found) {
@@ -59,8 +78,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void showBottomSheetDialog() {
-        final DialogFragment dialogFragment = new DialogFragment();
-        dialogFragment.show(getSupportFragmentManager(), dialogFragment.getTag());
+//    private void showBottomSheetDialog() {
+//        final DialogFragment dialogFragment = new DialogFragment();
+//        dialogFragment.show(getSupportFragmentManager(), dialogFragment.getTag());
+//    }
+
+    private void setupBottomSheet() {
+        bottomSheetToolbar.setTitle(R.string.bottom_sheet_title);
+        final PagerAdapter sectionsPagerAdapter = new PagerAdapter(getSupportFragmentManager(), this, TabItem.CONNECTION);
+        bottomSheetViewPager.setOffscreenPageLimit(1);
+        bottomSheetViewPager.setAdapter(sectionsPagerAdapter);
+        bottomSheetTabLayout.setupWithViewPager(bottomSheetViewPager);
+        BottomSheetUtils.setupViewPager(bottomSheetViewPager);
+    }
+
+    public void bluetoothOnClickMethods(View v) {
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            if (fragment instanceof BluetoothFragment) {
+                BluetoothFragment bluetooth_fragment = (BluetoothFragment) fragment;
+                bluetooth_fragment.myClickMethod(v, this);
+            }
+        }
     }
 }
