25e005b95c1f38a0e26570d4429415a6fd3595b4
Keane <keanecodes@gmail.com>
2021-10-14 09:42:57 +0800

    i guess im done

--- app/src/main/java/ntu/scse/mdp13/leaderboard/MapConfigDialog.java
+++ app/src/main/java/ntu/scse/mdp13/leaderboard/MapConfigDialog.java
@@ -7,10 +7,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
@@ -39,25 +42,24 @@ public class MapConfigDialog extends ViewPagerBottomSheetDialogFragment {
 
         //getActivity().getPreferences(Context.MODE_PRIVATE).edit().clear().commit(); //clear all saved obs
         MapCanvas _map = (MapCanvas) getActivity().findViewById(R.id.pathGrid);
+        if (_map.getFinder().getTargets().size() <= 0) saveBtn.setVisibility(View.GONE);
 
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        saveBtn.setOnClickListener(view -> {
+            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
 
-                int n = 0;
-                String message = "";
-                while (n < _map.getFinder().getTargets().size()) {
-                    message += (n+1) + "," + _map.getFinder().getTargets().get(n).getX() + "," + (21-_map.getFinder().getTargets().get(n).getY()) + "," + _map.getFinder().getTargets().get(n).getF() + "|";
-                    n++;
-                }
+            int n = 0;
+            String message = "";
+            while (n < _map.getFinder().getTargets().size()) {
+                message += (n+1) + "," + _map.getFinder().getTargets().get(n).getX() + "," + (21-_map.getFinder().getTargets().get(n).getY()) + "," + _map.getFinder().getTargets().get(n).getF() + "|";
+                n++;
+            }
 
-                if (!message.equals("")) {
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString(message, message);
-                    editor.apply();
-                    readSavedMaps(_map);
-                }
+            if (!message.equals("")) {
+                message = StringUtils.removeEnd(message, "|");
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(message, message);
+                editor.apply();
+                readSavedMaps(_map);
             }
         });
         readSavedMaps(_map);
@@ -70,26 +72,28 @@ public class MapConfigDialog extends ViewPagerBottomSheetDialogFragment {
         for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
             Button button = new Button(getContext());
             button.setText(entry.getValue().toString());
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String msg = (String) button.getText();
-                    String[] obss = msg.split("\\|");
-                    _map.getFinder().resetGrid();
-                    for(int i=0; i <= obss.length-1 ; i++) {
-                        String[] xy = obss[i].split(",");
-                        _map.getFinder().getTargets().add(new Target(Integer.parseInt(xy[1]), 21-Integer.parseInt(xy[2]), Integer.parseInt(xy[0]), Integer.parseInt(xy[3])));
-                        _map.getFinder().getBoard()[_map.getFinder().getTargets().get(i).getX()][_map.getFinder().getTargets().get(i).getY()] = TARGET_CELL_CODE;
-                    }
+            button.setBackgroundColor(getResources().getColor(R.color.black_bg));
+            button.setTextColor(getResources().getColor(R.color.blue_hl));
+            ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(0, 0, 0, 5);
+            button.setLayoutParams(params);
+            button.setOnClickListener(view -> {
+                String msg = (String) button.getText();
+                String[] obss = msg.split("\\|");
+                _map.getFinder().resetGrid();
+                for(int i=0; i <= obss.length-1 ; i++) {
+                    String[] xy = obss[i].split(",");
+                    _map.getFinder().getTargets().add(new Target(Integer.parseInt(xy[1]), 21-Integer.parseInt(xy[2]), Integer.parseInt(xy[0]), Integer.parseInt(xy[3])));
+                    _map.getFinder().getBoard()[_map.getFinder().getTargets().get(i).getX()][_map.getFinder().getTargets().get(i).getY()] = TARGET_CELL_CODE;
                 }
             });
-            button.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    sharedPref.edit().remove((String) button.getText()).commit();
-                    linearLayout.removeView(button);
-                    return true;
-                }
+            button.setOnLongClickListener(view -> {
+                sharedPref.edit().remove((String) button.getText()).commit();
+                linearLayout.removeView(button);
+                return true;
             });
             linearLayout.addView(button);
         }
